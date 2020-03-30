ded1fc05dc56daad93b24fa9d5cc45818fe6dacb
james <Jamestangdeveloper@gmail.com>
2020-03-30 23:50:08 +0100

    change table to env

--- create.js
+++ create.js
@@ -5,7 +5,7 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
-    TableName: "notes",
+    TableName: process.env.tableName,
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: uuid.v1(),
@@ -19,58 +19,7 @@ export async function main(event, context) {
     await dynamoDbLib.call("put", params);
     return success(params.Item);
   } catch (e) {
-    console.log("e", e);
+    console.log("error", e);
     return failure({ status: false });
   }
 }
-
-// const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-// export function main(event, context, callback) {
-//   //parse() data becomes a javascript object
-//   const data = JSON.parse(event.body);
-
-//   const params = {
-//     TableName: process.env.tableName,
-//     Item: {
-//       //'userId': user identities are federated through the
-//       // Cognito Identity Pool, we will use the identity id
-//       // as the user id of the authenticated user
-//       userId: event.requestContext.identity.cognitoIdentityId,
-//       // userId: "USER-SUB-1234",
-//       noteId: uuid.v1(),
-//       content: data.content,
-//       attachment: data.attachment,
-//       createdAt: Date.now()
-//     }
-//   };
-
-//   dynamoDb.put(params, (error, data) => {
-//     const headers = {
-//       "Access-Control-Allow-Origin": "*",
-//       "Access-Control-Allow-Credentials": true
-//     };
-
-//     console.log("data", data);
-//     console.log("params", params.Item);
-//     console.log("error", error.message);
-
-//     if (error) {
-//       const response = {
-//         statusCode: 500,
-//         headers: headers,
-//         body: JSON.stringify({ status: false })
-//       };
-//       callback(null, response);
-//       return;
-//     }
-
-//     // Return status code 200 and the newly created
-//     const response = {
-//       statusCode: 200,
-//       headers: headers,
-//       body: JSON.stringify(params.Item)
-//     };
-//     callback(null, response);
-//   });
-// }
